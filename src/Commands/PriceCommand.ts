60dd09f256b4360df23967f0efc986e856191be5
xicor333 <immortalxicor@gmail.com>
2022-01-30 07:24:55 -0600

    Fix inv string output

--- src/Commands/PriceCommand.ts
+++ src/Commands/PriceCommand.ts
@@ -1,11 +1,17 @@
-import { CommandInteraction, MessageEmbed,MessageAttachment, ApplicationCommand, Constants } from "discord.js";
+import {
+  CommandInteraction,
+  MessageEmbed,
+  MessageAttachment,
+  ApplicationCommand,
+  Constants,
+} from "discord.js";
 import { BasicCommand } from "./BasicCommand";
 import { TinychartAPI } from "../tinychartAPI";
 import { Asset, Pool } from "../tinychart";
 
 export class PriceCommand extends BasicCommand {
   constructor() {
-    super(["tcp","price"]);
+    super(["tcp", "price"]);
   }
   async runCommand(interaction: CommandInteraction): Promise<void> {
     const { commandName, options } = interaction;
@@ -26,7 +32,9 @@ export class PriceCommand extends BasicCommand {
       })
       .then((info) => {
         if (!info.pools || info.pools.length < 1)
-          throw new Error(`No pools found for ${info.targetAsset.ticker} on ${info.provider}`);
+          throw new Error(
+            `No pools found for ${info.targetAsset.ticker} on ${info.provider}`
+          );
         //find the algo -> asa pool and return the price on that pool
         const pool = TinychartAPI.getAlgoPool(info.pools);
         if (!pool) {
@@ -35,43 +43,51 @@ export class PriceCommand extends BasicCommand {
           );
         }
         const pctChange = ((pool.price - pool.price24h) / pool.price24h) * 100;
-        const pctChangeStr = (pctChange<0?"":"+")+pctChange.toFixed(2);
-        const priceStr = (inv?1/pool.price:pool.price).toPrecision(4);
-        const icons =this.footerIcons(info.targetAsset)
+        const pctChangeStr = (pctChange < 0 ? "" : "+") + pctChange.toFixed(2);
+        const priceCalc = inv ? 1 / pool.price : pool.price;
+        let priceStr =
+          (priceCalc >= 10000
+            ? priceCalc.toFixed(0)
+            : priceCalc.toPrecision(4)) + (inv ? " Per " : "");
+        const icons = this.footerIcons(info.targetAsset);
         const embed = {
-            title:`${info.targetAsset.name}`,
-            fields:[
-                {name:"Asset",value:`${info.targetAsset.ticker}`,inline:true},
-                {name:"Price",value:`${priceStr}Ⱥ`,inline:true},
-                {name:"Change",value:`${pctChangeStr}%`,inline:true}
-            ],
-            footer:{
-                text:`From ${info.provider}\n${icons}`
-            }
-        }
+          title: `${info.targetAsset.name}`,
+          fields: [
+            {
+              name: "Asset",
+              value: `${info.targetAsset.ticker}`,
+              inline: true,
+            },
+            { name: "Price", value: `${priceStr}Ⱥ`, inline: true },
+            { name: "Change", value: `${pctChangeStr}%`, inline: true },
+          ],
+          footer: {
+            text: `From ${info.provider}\n${icons}`,
+          },
+        };
         // interaction.editReply(
         //   `${info.targetAsset.ticker} Price on ${info.provider} is ${priceStr} <a>(${pctChangeStr}%)`
         // );
-        interaction.editReply({embeds:[embed]});
+        interaction.editReply({ embeds: [embed] });
       });
   }
-  buildDiscordCommands():ApplicationCommand[]{
-    let cmds=[];
-    for(const name of this.m_names){
-      cmds.push( {
+  buildDiscordCommands(): ApplicationCommand[] {
+    let cmds = [];
+    for (const name of this.m_names) {
+      cmds.push({
         name: name,
         description: "Replies with the price for the specified ASA",
         options: [
-          this.asaArgument(), 
+          this.asaArgument(),
           this.dexArgument(),
           {
             name: "inv",
             description: "Invert price",
             required: false,
             type: Constants.ApplicationCommandOptionTypes.BOOLEAN,
-          }
+          },
         ],
-      },)
+      });
     }
     return cmds;
   }
