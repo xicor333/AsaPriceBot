e22e76c76f33dfea7454048edef84a388bc99253
BunsanMuchi <bunsanmuchi@gmail.com>
2022-02-10 14:36:03 +0100

    Started work on easter egg

--- src/Commands/ChartCommand.ts
+++ src/Commands/ChartCommand.ts
@@ -2,9 +2,9 @@ import { CommandInteraction, MessageEmbed,MessageAttachment, ApplicationCommand,
 import { BasicCommand } from "./BasicCommand";
 import { TinychartAPI } from "../tinychartAPI";
 import { Asset, Pool, TimeQuery } from "../tinychart";
-import * as child_process from "child_process";
-import fs from  "fs";
+import mergeImages from "merge-images"
 import puppeteer, { Puppeteer, WaitTask } from "puppeteer";
+import canvas from "canvas";
 
 export class ChartCommand extends BasicCommand {
   constructor() {
@@ -16,7 +16,7 @@ export class ChartCommand extends BasicCommand {
     const asa: string = options.getString("asa");
     const timeNumber: number = options.getNumber("time");
     const dex: string | null = options.getString("dex");
-    const inv: boolean = options.getBoolean("inv");
+    const ee: boolean = options.getBoolean("ee");
 
     await interaction.deferReply();
     return TinychartAPI.getAsset(asa)
@@ -45,74 +45,84 @@ export class ChartCommand extends BasicCommand {
           await page.goto(url);                      
 
 
-          await page.waitForTimeout(1000); 
+          await page.waitForTimeout(800); 
 
-          await page.mouse.click(1140, 200, {button: "left"}  )
+          await page.mouse.click(1140, 200, {button: "left"}  );
           
-          await page.mouse.move(1140, 280)
+          await page.mouse.move(1140, 280);
 
-          await page.mouse.wheel({deltaY: (timeNumber % 2 > 0 ) ?  - 1000 : 1000})
+          await page.mouse.wheel({deltaY: (timeNumber % 2 > 0 ) ?  - 1000 : 1000});
 
-          const x = 1128
-          const yDelta = 32
-          const yStart = ((timeNumber % 2 > 0 ) ?   271 : 233)
+          const x = 1128;
+          const yDelta = 32;
+          const yStart = ((timeNumber % 2 > 0 ) ?   271 : 233);
+          const timeWait = 200;
           
           switch(timeNumber){
             case (1) :
               
-              await page.mouse.click(x, yStart + yDelta, {button: "left"}  )
-              await page.waitForTimeout(500)
+              await page.mouse.click(x, yStart + yDelta, {button: "left"}  );
+              await page.waitForTimeout(timeWait);
               break;
             case (3) :
-              await page.mouse.click(x, yStart + yDelta * 2, {button: "left"}  )
-              await page.waitForTimeout(500)
+              await page.mouse.click(x, yStart + yDelta * 2, {button: "left"}  );
+              await page.waitForTimeout(timeWait);
               break;
             case (5) :
-              await page.mouse.click(x, yStart + yDelta * 3, {button: "left"}  )
-              await page.waitForTimeout(500)
+              await page.mouse.click(x, yStart + yDelta * 3, {button: "left"}  );
+              await page.waitForTimeout(timeWait);
               break;
             case (7) :
-              await page.mouse.click(x, yStart + yDelta * 4, {button: "left"}  )
-              await page.waitForTimeout(500)
+              await page.mouse.click(x, yStart + yDelta * 4, {button: "left"}  );
+              await page.waitForTimeout(timeWait);
               break;
             case (9) :
-              await page.mouse.click(x, yStart + yDelta * 6, {button: "left"}  )
-              await page.waitForTimeout(500)
+              await page.mouse.click(x, yStart + yDelta * 6, {button: "left"}  );
+              await page.waitForTimeout(timeWait);
               break;
             case (2) :
-              await page.mouse.click(x, yStart + yDelta, {button: "left"}  )
-              await page.waitForTimeout(500)
+              await page.mouse.click(x, yStart + yDelta, {button: "left"}  );
+              await page.waitForTimeout(timeWait);
               break;
             case (4) :
-              await page.mouse.click(x, yStart + yDelta * 2, {button: "left"}  )
-              await page.waitForTimeout(500)
+              await page.mouse.click(x, yStart + yDelta * 2, {button: "left"}  );
+              await page.waitForTimeout(timeWait);
               break;            
             case (6) :
-              await page.mouse.click(x, yStart + yDelta * 3, {button: "left"}  )
-              await page.waitForTimeout(500)
+              await page.mouse.click(x, yStart + yDelta * 3, {button: "left"}  );
+              await page.waitForTimeout(timeWait);
               break;
             case (8) :
-              await page.mouse.click(x, yStart + yDelta * 6, {button: "left"}  )
-              await page.waitForTimeout(500)
+              await page.mouse.click(x, yStart + yDelta * 6, {button: "left"}  );
+              await page.waitForTimeout(timeWait);
               break;
             case (10) :
-              await page.mouse.click(x, yStart + yDelta * 7, {button: "left"}  )
-              await page.waitForTimeout(500)
+              await page.mouse.click(x, yStart + yDelta * 7, {button: "left"}  );
+              await page.waitForTimeout(timeWait);
               break;}
-
-
             
           await page.screenshot({
             path: `${chart.targetAsset.id}.png`,
-            clip: {x: 199, y: 195, width: 1130, height: 820}})
+            clip: {x: 199, y: 195, width: 1130, height: 820}});
         
           await browser.close();          
 
            
         }                  
         await Screenshot();
+
+        if(ee){
+          const pctChange: boolean = ((chart.pools[0].price - chart.pools[0].price24h) >= 0 ? true:false)
+          if(pctChange){
+            mergeImages([`${chart.targetAsset.id}.png`, "./easter_egg/bera1.png", "./easter_egg/bera2.png"])
+          }
+
+          else{
+            mergeImages([`${chart.targetAsset.id}.png`, "./easter_egg/bera1.png", "./easter_egg/bera2.png"])
+          }
+        }
         
-        const file = new MessageAttachment(`${chart.targetAsset.id}.png`)
+        const file = new MessageAttachment(`${chart.targetAsset.id}.png`)        
 
         const embed = {
             title:`Chart`,
@@ -150,12 +160,12 @@ export class ChartCommand extends BasicCommand {
             ]
           },
           this.dexArgument(),
-          /*{
-            name: "inv",
-            description: "Invert price",
+          {
+            name: "ee",
+            description: "Easter Egg",
             required: false,
             type: Constants.ApplicationCommandOptionTypes.BOOLEAN,
-          }*/
+          }
         ],
       },)
     }
